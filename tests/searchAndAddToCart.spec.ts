c87811f9576cba3f9bf931a13fd79b7c42b3cd60
eckhoffelijah2-boop <eckhoffelijah2@gmail.com>
2026-04-03 00:11:38 +0200

    Update and rename airpodsAmazonPOM.spec.ts to searchAndAddToCart.spec.ts

--- tests/searchAndAddToCart.spec.ts
+++ tests/searchAndAddToCart.spec.ts
@@ -1,7 +1,7 @@
 import { test,expect,Locator,Page } from '@playwright/test';
 import {POManager} from '../POMts/POManager';
 
-test('add 2 AirPods to basket', async ({ page }) => {
+test('searchAndAddToCart', async ({ page }) => {
   const poManager = new POManager(page);
   const amazonHomePage = poManager.getAmazonHomePage();
   const airPodsPage = poManager.getAirPodsPage();
